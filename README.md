f2fb41dd57b3fd3a767f583830508573675c44c7
iujisato <iuji.ujisato@gmail.com>
2016-04-19 15:46:14 -0300

    Update README.md
    * Change interface messages.
    * Update README.md
    * Refactor code to prevent some special users breaking the game.

--- README.md
+++ README.md
@@ -3,15 +3,19 @@ Refactor Hangman's Clojure to Elixir.
 
 <details>
 <summary>Its pretty easy to guess the secret word, but if you dont want to have some fun click on the left arrow</summary>
-The secret word for the clojure's hangman is `elixir`
+The secret word for the clojure's hangman is `Elixir`
 </details>
 
 
 ## Requirements
-...
+* Erlang/OTP 18.3
+* Elixir 1.2.3
 
 ## Usage
-...
-
-## FAQ
-...
+1. `git clone` the project.
+2. Inside `(your/git/clone/project)/Elixir` run:
+```
+iex -S mix
+Hangman.game
+```
+and have fun!
--- lib/hangman.ex
+++ lib/hangman.ex
@@ -1,28 +1,11 @@
 defmodule Hangman do
-  # iex> Benchmark.measure(fn -> Hangman.game2(600_000) end)
-  # 11.215590
-  # 11.312048
-  # 11.276873
-
-  # Returns keyword mapset with characters
-  # Hangman.keyword_letters
-  # Tell me a secret keyword! Banana
-  # MapSet<["B", "a", "n"]>
   def keyword_letters(keyword) do
     blocked_chars = MapSet.new(["", "\n", " "])
 
-    String.split(String.upcase(keyword), "")
+    String.split(keyword, "")
       |> MapSet.new
       |> MapSet.difference(blocked_chars)
   end
-
-  # (defn feedback-interface [remaining-chances word right-guesses]
-  #   (println "Remaining chances:" remaining-chances)
-  #   (doseq [character (seq word)]
-  #   (if (contains? right-guesses (str character))
-  #       (print character " ")
-  #       (print "_" " ")))
-  #   (println))
 
   def feedback_interface(remaining_chances, keyword, right_guesses) do
     IO.puts "Remaining chances: #{remaining_chances}"
@@ -48,58 +31,31 @@ defmodule Hangman do
     end
   end
 
-
   def game2(0, _, _) do
-    IO.puts "Você perdeu!"
+    IO.puts "Well, shit happens..."
   end
 
   def game2(remaining_chances, keyword, right_guesses) do
+    keyword = String.upcase(keyword)
     feedback_interface(remaining_chances, keyword, right_guesses)
     cond do
       MapSet.equal?(right_guesses, keyword_letters(keyword)) ->
-        IO.puts "Você ganhou!"
+        IO.puts "Really? gratz bro!"
 
       true ->
         guess = String.strip(IO.gets "chuta aí ")
                   |> String.upcase
         if MapSet.member?(keyword_letters(keyword), guess) do
-          IO.puts "Acertou mizeráavi"
+          IO.puts "You guessed it right, mizeráavi!"
           game2(remaining_chances, keyword, MapSet.put(right_guesses, guess))
         else
-          IO.puts "Errou"
+          IO.puts "Oops, best luck next time!"
           game2((remaining_chances - 1), keyword, right_guesses)
         end
     end
   end
 
   def game do
-    game2(3, "OVO BRANCO", MapSet.new)
+    game2(3, "Elixir", MapSet.new)
   end
-  # iex> Benchmark.measure(fn -> Hangman.game1(600_000) end)
-  # 11.394374
-  # 11.410963
-  # 11.473353
-  # def game1(remaining_chances) do
-  #   cond do
-  #     remaining_chances <= 0 ->
-  #       IO.puts "Você perdeu!"
-  #     true ->
-  #       IO.puts remaining_chances
-  #       game1(remaining_chances - 1)
-  #   end
-  # end
-
-  # iex> Benchmark.measure(fn -> Hangman.game3(600_000) end)
-  # 11.306187
-  # 11.613859
-  # 11.370993
-  # def game3(remaining_chances) do
-  #   case remaining_chances do
-  #     remaining_chances when remaining_chances <= 0 ->
-  #       IO.puts "Você perdeu!"
-  #     _ ->
-  #       IO.puts remaining_chances
-  #       game3(remaining_chances - 1)
-  #   end
-  # end
 end
